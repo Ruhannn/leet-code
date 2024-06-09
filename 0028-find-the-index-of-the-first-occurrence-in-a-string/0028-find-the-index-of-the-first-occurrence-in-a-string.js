a3193097efd83f89449d7847c788e76ae4cc0a8d
Ruhan Rouf <kamiayaka09@Gmail.com>
2024-06-10 01:18:14 +0600

    Time: 47 ms (84.07%), Space: 49.2 MB (20.44%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length == 0) return 0;
+    for (let i = 0; i < haystack.length; i++) {
+        let k = i, j = 0;
+        while (haystack[k] == needle[j] && j < needle.length) {
+            k++, j++;
+        }
+        if (j == needle.length) return i;
+    }
+    return -1; 
+}
