1cc732b46f1b92777255be148f97ffd6cb5a8c53
Ruhan Rouf <Ruhanrouf17@gmail.com>
2024-07-29 13:14:18 +0600

    Time: 76 ms (48.42%), Space: 52.4 MB (81.87%)

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -7,7 +7,11 @@ var reverse = function (x) {
     const ns = x.toString()
     let r = ns.split('').reverse().join('');
     let num = parseInt(r)
+    // if (r.endsWith("-")) {
+    //     console.log(-Math.abs(parseInt(r)));
+    // }
     if (num > 2147483647)
         return 0
     return r.endsWith("-") ? -Math.abs(num) : num
+    // console.log(parseInt(r));
 };
